208018876a2353ae3317826ad3d6cd733cf761e0
Ashank Gupta <142603084+Ashank007@users.noreply.github.com>
2024-07-22 20:20:33 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <link rel="icon" href="/MIET.png" />
+      <link rel="icon" href="/favicon.ico" />
       <Chakra>
         {children}
       </Chakra>
