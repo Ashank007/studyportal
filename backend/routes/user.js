d094c286e35db70f05e7a925d660b4770c3d906b
Ashank Gupta <142603084+Ashank007@users.noreply.github.com>
2024-07-23 20:58:03 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,12 @@
 import express from "express"
 import { registeruser, loginuser, logoutuser, adminlogin, getallusers } from "../controllers/user.js";
 import isAuthenticated from "../middlewares/isauthenticated.js";
-import authenticateLoginToken from "../middlewares/authenticationtoke.js";
 const userrouter = express.Router();
 
 userrouter.post("/register", registeruser)
 userrouter.post("/login", loginuser)
 userrouter.get("/logout", isAuthenticated, logoutuser);
 userrouter.post("/admin", adminlogin);
-userrouter.get("/admin/getall",authenticateLoginToken, getallusers);
+userrouter.get("/admin/getall", getallusers);
 export default userrouter
 
