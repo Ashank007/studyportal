c7eb9d75741ac04cf09d0a40ec0162c3dfc10505
Ashank Gupta <142603084+Ashank007@users.noreply.github.com>
2024-07-23 20:41:26 +0530

    Update authenticationtoke.js

--- backend/middlewares/authenticationtoke.js
+++ backend/middlewares/authenticationtoke.js
@@ -1,9 +1,8 @@
 const authenticateLoginToken = (req, res, next) => {
-    const token = req.cookies.LOGIN_TOKEN || req.session.LOGIN_TOKEN;
+    const {token} = req.cookies;
     if (!token) {
         return res.status(401).json(new ApiError(false, 'Access Denied: No Token Provided'));
     }
     next();
 };
-
 export default authenticateLoginToken;
