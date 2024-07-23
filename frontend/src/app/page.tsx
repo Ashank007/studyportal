48713c83bd78b63ad1ae6b7212d37506b45bd446
Ashank Gupta <142603084+Ashank007@users.noreply.github.com>
2024-07-23 18:05:51 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <Flex direction={{lg:'row', base:'column'}} minH={'100vh'}  maxW='100vw' bgSize={"cover"} bgRepeat={'no-repeat'} justifyContent={"right"} alignItems={'center'} p={10}
       backgroundPosition="left" backgroundImage="url('https://img.freepik.com/free-photo/blue-surface-with-study-tools_23-2147864592.jpg?w=1060&t=st=1721140280~exp=1721140880~hmac=1d3020c0c8dca264347fab00b306780b985e26a25d437c479537f38810c25aa1')">
         <VStack spacing={5}>
-          <AnimatedText text='Welcome to MIET Material Portal'/>
+          <AnimatedText text='Welcome to Material Portal'/>
           <Flex direction={['column','column','column','row']} w={'fit-content'} gap={'20px'} justifyContent={'center'} alignItems={'center'}>
           <Box boxShadow='dark-lg' border={'2px'} borderColor={'black'} rounded={'6px'} position={'relative'} width={['90vw','90vw','500px']} height={'300px'}>
             <HomeImage url='homeimg.avif'/>
